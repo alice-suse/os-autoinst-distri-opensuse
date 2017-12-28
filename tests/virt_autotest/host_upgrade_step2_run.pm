40624255869bd2924296e968ed7baa74edbec3cc
alice <xlai@suse.com>
2017-12-28 19:19:10 +0800

    Virtualization: replace host upgrade autoyast module urls with openqa daily build links

--- tests/virt_autotest/host_upgrade_step2_run.pm
+++ tests/virt_autotest/host_upgrade_step2_run.pm
@@ -14,6 +14,7 @@ use base "host_upgrade_base";
 #use virt_utils qw(set_serialdev);
 use testapi;
 use strict;
+use virt_utils;
 
 sub get_script_run {
     my $self = shift;
@@ -28,6 +29,14 @@ sub run {
     my $self = shift;
     $self->run_test(12600, "Host upgrade to .* is done. Need to reboot system|Executing host upgrade to .* offline",
         "no", "yes", "/var/log/qa/", "host-upgrade-prepAndUpgrade");
+
+    #replace module url with openqa daily build modules link
+    my $upgrade_product = get_required_var('UPGRADE_PRODUCT');
+    $upgrade_product =~ /sles-([0-9]+)-sp/;
+    $upgrade_release = $1;
+    if ($upgrade_release >= 15) {
+        repl_addon_with_daily_build_module_in_files('/usr/share/qa/virtautolib/data/autoupg_template_with_module.xml');
+    }
 }
 
 1;

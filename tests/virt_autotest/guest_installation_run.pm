f7eb96e687f0cb2a00ae1578e93af3c91460e13e
alice <xlai@suse.com>
2019-03-22 10:32:01 +0800

    [Virtualization]: support UPLOAD_GUEST_ASSETS

--- tests/virt_autotest/guest_installation_run.pm
+++ tests/virt_autotest/guest_installation_run.pm
@@ -30,6 +30,10 @@ sub get_script_run {
     my $guest_pattern = get_var('GUEST_PATTERN', 'sles-12-sp2-64-[p|f]v-def-net');
     my $parallel_num  = get_var("PARALLEL_NUM",  "2");
     $pre_test_cmd = $pre_test_cmd . " -f " . $guest_pattern . " -n " . $parallel_num . " -r ";
+    
+#    $pre_test_cmd = "cd /usr/share/qa/virtautolib/lib;source virtlib;install_vm_guests '' $guest_pattern";
+    #TODO, REMOVE DEBUG
+#    $pre_test_cmd = 'cat /tmp/debug.quick-output';
 
     return $pre_test_cmd;
 }
@@ -51,11 +55,34 @@ sub analyzeResult {
 sub run {
     my $self = shift;
 
+    assert_script_run("sed -i 's/vm-install.sh/vm-install\.sh -g /' /usr/share/qa/qa_test_virtualization/installos");
+    assert_script_run("sed -i 's/virt-install.sh/virt-install\.sh -u /' /usr/share/qa/qa_test_virtualization/virt_installos");
+    assert_script_run('cat /usr/share/qa/qa_test_virtualization/installos | grep vm-install');
+    save_screenshot;
+    assert_script_run('cat /usr/share/qa/qa_test_virtualization/virt_installos | grep virt-install');
+    save_screenshot;
+
     $self->{"product_tested_on"} = "SLES-12-SP2";
     $self->{"product_name"}      = "GuestIn_stallation";
     $self->{"package_name"}      = "Guest Installation Test";
+    $self->{success_guest_list} = [];
 
-    $self->run_test(7600, "", "yes", "yes", "/var/log/qa/", "guest-installation-logs");
+    my $upload_guest_assets_flag = 'no';
+    if (check_var('UPLOAD_GUEST_ASSETS', '1')) {
+        $upload_guest_assets_flag = 'yes';
+    }
+
+    #$self->run_test(7600, "", "yes", "yes", "/var/log/qa/", "guest-installation-logs");
+    $self->run_test(7600, "", "yes", "yes", "/var/log/qa/", "guest-installation-logs", $upload_guest_assets_flag);
+
+#    if (check_var('UPLOAD_ASSETS', 1)) {
+#        upload_asset("/var/lib/libvirt/images/sles-15-sp1-64-fv-def-net-build158-alice.qcow2", 1, 1);
+#    }
+#
+#    if (check_var('SKIP_GI', 1)) {
+#        #assert_script_run("mkdir -p /tmp/osd; mount -t nfs openqa.suse.de:/var/lib/openqa/share/factory /tmp/osd; ls /tmp/osd/hdd");
+#        assert_script_run("mkdir -p /tmp/osd; mount -t nfs 10.67.18.220:/var/lib/openqa/share/factory /tmp/osd; ls /tmp/osd/hdd;umount -l /tmp/osd");
+#    }
 }
 
 1;
--- tests/virt_autotest/virt_autotest_base.pm
+++ tests/virt_autotest/virt_autotest_base.pm
@@ -45,6 +45,7 @@ sub generateXML {
     foreach my $item (keys(%my_hash)) {
         if ($my_hash{$item}->{status} =~ m/PASSED/) {
             $pass_nums += 1;
+            push @{$self->{success_guest_list}}, $item;
         }
         elsif ($my_hash{$item}->{status} =~ m/SKIPPED/ && $item =~ m/iso/) {
             $skip_nums += 1;
@@ -53,6 +54,9 @@ sub generateXML {
             $fail_nums += 1;
         }
     }
+
+    diag '@{$self->{success_guest_list}} content is: ' . Dumper(@{$self->{success_guest_list}});
+
     my $count = $pass_nums + $fail_nums + $skip_nums;
     $writer->startTag(
         'testsuites',
@@ -147,10 +151,12 @@ sub push_junit_log {
 }
 
 sub run_test {
-    my ($self, $timeout, $assert_pattern, $add_junit_log_flag, $upload_virt_log_flag, $log_dir, $compressed_log_name) = @_;
+    my ($self, $timeout, $assert_pattern, $add_junit_log_flag, $upload_virt_log_flag, $log_dir, $compressed_log_name, $upload_guest_assets_flag) = @_;
     if (!$timeout) {
         $timeout = 300;
     }
+
+    #$self->{success_guest_list} = [];
 
     my $test_cmd      = $self->get_script_run();
     my $script_output = $self->execute_script_run($test_cmd, $timeout);
@@ -161,6 +167,11 @@ sub run_test {
 
     if ($upload_virt_log_flag eq "yes") {
         upload_virt_logs($log_dir, $compressed_log_name);
+    }
+
+    if ($upload_guest_assets_flag eq "yes") {
+        record_info('Check UPLOAD_GUEST_ASSETS flag', 'This test should upload guest assets!');
+        $self->upload_guest_assets;
     }
 
     if ($assert_pattern) {
@@ -191,6 +202,80 @@ sub upload_virt_logs {
     upload_logs "$full_compressed_log_name.gz";
     save_screenshot;
 
+}
+
+# Guest xml will be uploaded with name format [generated_name_by_this_func].xml
+# Guest disk will be uploaded with name format [generated_name_by_this_func].disk
+# When reusing these assets, needs to recover the names to original by reverting this process
+sub generate_guest_asset_name {
+    my $guest = shift;
+
+    my $composed_name = 'guest_' . $guest . '_on-host_' . get_required_var('DISTRI') . '-' . get_required_var('VERSION') 
+                        . '_build' . get_required_var('BUILD') . '_' . lc(get_required_var('SYSTEM_ROLE')) . '_' . get_required_var('ARCH');
+
+    record_info('Guest asset info', "Guest asset name is : $composed_name");
+
+    return $composed_name;
+}
+
+sub get_guest_disk_name_from_guest_xml {
+    my $guest = shift;
+
+    # Our automation only supports single guest disk
+    my $disk_from_xml = script_output("virsh dumpxml $guest | sed -n \'/disk/,/\\\/disk/p\' | grep 'source file=' | grep -v iso");
+    record_info('Guest disk config from xml', "Guest $guest disk_from_xml is: $disk_from_xml.");
+    $disk_from_xml =~ /file='(.*)'/;
+    $disk_from_xml = $1;
+    die 'There is no guest disk file parsed out from guest xml configuration!' unless $disk_from_xml;
+    record_info('Guest disk name', "Guest $guest disk_from_xml is: $disk_from_xml.");
+
+    return $disk_from_xml;
+}
+
+# Should only do compress from qcow2 disk to qcow2 in our automation(upload guest asset scheme).
+sub compress_single_qcow2_disk {
+    my ($orig_disk, $compressed_disk) = @_;
+
+    if ($orig_disk =~ /qcow2/) {
+        my $cmd = "nice ionice qemu-img convert -c -p -O qcow2 $orig_disk $compressed_disk";
+        assert_script_run($cmd, 360);
+        save_screenshot;
+        record_info('Disk compression', "Disk compression done from $orig_disk to $compressed_disk.");
+    }
+}
+
+sub upload_guest_assets {
+    my $self = shift;
+
+    record_info('Skip upload guest asset.', 'No successful guest, skip upload assets.') unless @{$self->{success_guest_list}};
+
+    foreach my $guest (@{$self->{success_guest_list}}) {
+        # Generate upload guest asset name
+        my $guest_upload_asset_name = generate_guest_asset_name($guest);
+        # Upload guest xml
+        my $guest_xml_name = $guest_upload_asset_name . '.xml';
+        assert_script_run("virsh dumpxml $guest > /tmp/$guest_xml_name");
+        upload_asset("/tmp/$guest_xml_name", 1, 1);
+        assert_script_run("rm /tmp/$guest_xml_name");
+        record_info('Guest xml upload done', "Guest $guest xml uploaded as $guest_xml_name.");
+        # Upload guest disk
+        # Uploaded guest disk name is different from original disk name in guest xml.
+        # This is to differentiate guest disk on different host, hypervisor and product build.
+        # Need to recover the disk name when recovering guests from openqa assets.
+        my $guest_disk_name_real = get_guest_disk_name_from_guest_xml($guest);
+        my $guest_disk_name_to_upload = $guest_upload_asset_name . '.disk';
+        if ($guest_disk_name_real =~ /qcow2/) {
+            # Disk compression only for qcow2
+            compress_single_qcow2_disk($guest_disk_name_real, $guest_disk_name_to_upload);
+        }
+        else {
+            # Link real disk to uploaded disk name to be with needed name after upload
+            assert_script_run("ln -s $guest_disk_name_real $guest_disk_name_to_upload");
+        }
+        upload_asset("$guest_disk_name_to_upload", 1, 0);
+        assert_script_run("rm $guest_disk_name_to_upload");
+        record_info('Guest disk upload done', "Guest $guest disk uploaded as $guest_disk_name_to_upload.");
+    }
 }
 
 1;
